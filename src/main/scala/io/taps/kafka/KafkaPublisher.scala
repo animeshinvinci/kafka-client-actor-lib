cf2c42c318e24d4439348b20d916a62f307abab8
animesh <animesh@ebctechnologies.com>
2017-10-28 23:11:16 +0530

    with transactional publishing to kafka topic

--- src/main/scala/io/taps/kafka/KafkaPublisher.scala
+++ src/main/scala/io/taps/kafka/KafkaPublisher.scala
@@ -9,6 +9,10 @@ import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import kafka.consumer.{Consumer, ConsumerConfig}
 import io.confluent.kafka.serializers.{KafkaAvroDecoder, KafkaAvroSerializer}
+import org.apache.kafka.common.KafkaException
+import org.apache.kafka.common.errors.AuthorizationException
+import org.apache.kafka.common.errors.OutOfOrderSequenceException
+import org.apache.kafka.common.errors.ProducerFencedException
 
 
 class KafkaPublisher {
@@ -26,11 +30,26 @@ class KafkaPublisher {
   producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer])
   producerProps.put("schema.registry.url", conf.getString("kafka.schema-registry"))
 
+
+
   val producer = new KafkaProducer[Object, Object](producerProps)
 
 
+
+  val  producerTxnProps = new Properties()
+  producerTxnProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, s"${conf.getString("kafka.broker")}")
+  producerTxnProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer])
+  producerTxnProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer])
+  producerTxnProps.put("schema.registry.url", conf.getString("kafka.schema-registry"))
+  producerTxnProps.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, UUID.randomUUID().toString) // this has to be set!!! (unique for each producer you're having)
+  producerTxnProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true") // has to be idempotent
+
+
+  val txnproducer = new KafkaProducer[Object, Object](producerTxnProps)
+
+
   def publish (topic: String, value: Object) ={
-    val rec = new ProducerRecord[Object,Object](topic,UUID.randomUUID() , Object)
+    val rec = new ProducerRecord[Object,Object](topic,UUID.randomUUID() , value)
     producer.send(rec)
 
   }
@@ -40,9 +59,20 @@ class KafkaPublisher {
 
   def publishTransactional(topic: String, value: Object) = {
 
-    producer.beginTransaction()
+    producer.initTransactions()
 
-    producer.commitTransaction()
+    try {
+      producer.beginTransaction()
+        producer.send(new ProducerRecord[Object, Object](topic,UUID.randomUUID() , value))
+      producer.commitTransaction()
+    } catch {
+      case e@(_: ProducerFencedException | _: OutOfOrderSequenceException | _: AuthorizationException) =>
+        // We can't recover from these exceptions, so our only option is to close the producer and exit.
+        producer.close()
+      case e: KafkaException =>
+        
+        producer.abortTransaction()
+    }
 
   }
 
